38e41503a05cdea5fa127cb0a6d0c1b61f638710
naldurien <nicole.aldurien@gmail.com>
2021-07-16 19:22:55 -0400

    spanish-english index pages

---
+++ menu.js
@@ -0,0 +1,485 @@
+meats = [Asada, Pastor,  Pollo, Chorizo, Lengua, Tripa, Carnitas, Barbacoa, Borrego, Cabeza, Milanesa de Pollo, Milanesa de Res]
+
+
+menu = [
+    {
+        "title": "Sope",
+        "description" : null,
+        "imageURL" : null",
+        "course" : "Starters",
+        "id" : 1,
+        "price" : 3.59
+    },
+    {
+        "title": "Ensalada de Pollo",
+        "description" : null,
+        "imageURL" : null",
+        "course" : "Starters",
+        "id" : 2,
+        "price" : 8.39
+    },
+    {
+        "title": "Tostada",
+        "description" : null,
+        "imageURL" : null",
+        "course" : "Starters",
+        "id" : 3,
+        "price" : 2.99
+    },
+    {
+        "title": "Nachos",
+        "description" : null,
+        "imageURL" : null",
+        "course" : "Starters",
+        "id" : 4,
+        "price" : 7.99
+    },
+    {
+        "title": "Chicken Fingers",
+        "description" : null,
+        "imageURL" : null",
+        "course" : "Starters",
+        "id" : 5,
+        "price" : 5.99
+    },
+    {
+        "title": "Tamal de Puerco",
+        "description" : null,
+        "imageURL" : null",
+        "course" : "Starters",
+        "id" : 6,
+        "price" : 2.39
+    },
+    {
+        "title": "Taco Salad",
+        "description" : null,
+        "imageURL" : null",
+        "course" : "Starters",
+        "id" : 7,
+        "price" : 7.91
+    },
+    {
+        "title": "Huarache",
+        "description" : null,
+        "imageURL" : null",
+        "course" : "Starters",
+        "id" : 8,
+        "price" : 7.19
+    },
+    {
+        "title": "Chimichanga",
+        "description" : null,
+        "imageURL" : null",
+        "course" : "Starters",
+        "id" : 9,
+        "price" : 8.27
+    },
+    {
+        "title": "Burrito Verde",
+        "description" : null,
+        "imageURL" : null",
+        "course" : "Starters",
+        "id" : 10,
+        "price" : 8.39
+    },
+    {
+        "title": "Flautas",
+        "description" : null,
+        "imageURL" : null",
+        "course" : "Starters",
+        "id" : 11,
+        "price" : 8.39
+    },
+    {
+        "title": "Quesadilla",
+        "description" : null,
+        "imageURL" : null",
+        "course" : "Starters",
+        "id" : 12,
+        "price" : 8.59
+    },
+    {
+        "title": "Torta",
+        "description" : null,
+        "imageURL" : null",
+        "course" : "Starters",
+        "id" : 13,
+        "price" : 7.91
+    },
+    {
+        "title": "Burrito La Villa",
+        "description" : null,
+        "imageURL" : null",
+        "course" : "Starters",
+        "id" : 14,
+        "price" : 8.39
+    },
+    {
+        "title": "Burrito Regular",
+        "description" : null,
+        "imageURL" : null",
+        "course" : "Starters",
+        "id" : 15,
+        "price" : 7.91
+    },
+    {
+        "title": "Tamal de Pollo",
+        "description" : null,
+        "imageURL" : null",
+        "course" : "Starters",
+        "id" : 16,
+        "price" : 2.39
+    },
+    {
+        "title": "Chips and Salsa",
+        "description" : null,
+        "imageURL" : null",
+        "course" : "Sides",
+        "id" : 17,
+        "price" : 1.79
+    },
+    {
+        "title": "Pico de Gallo",
+        "description" : null,
+        "imageURL" : null",
+        "course" : "Sides",
+        "id" : 18,
+        "price" : 1.79
+    },
+    {
+        "title": "Arroz",
+        "description" : null,
+        "imageURL" : null",
+        "course" : "Sides",
+        "id" : 19,
+        "price" : 1.67
+    },
+    {
+        "title": "Frijoles",
+        "description" : null,
+        "imageURL" : null",
+        "course" : "Sides",
+        "id" : 20,
+        "price" : 1.67
+    },
+    {
+        "title": "Aguacate",
+        "description" : null,
+        "imageURL" : null",
+        "course" : "Sides",
+        "id" : 20,
+        "price" : 1.71
+    },
+    {
+        "title": "Arroz y Frijoles",
+        "description" : null,
+        "imageURL" : null",
+        "course" : "Sides",
+        "id" : 21,
+        "price" : 1.91
+    },
+    {
+        "title": "6 Tortillas de Maiz",
+        "description" : null,
+        "imageURL" : null",
+        "course" : "Sides",
+        "id" : 22,
+        "price" : 0.71
+    },
+    {
+        "title": "6 Tortillas de Arina",
+        "description" : null,
+        "imageURL" : null",
+        "course" : "Sides",
+        "id" : 22,
+        "price" : 0.71
+    },
+    {
+        "title": "Chips and Queso",
+        "description" : null,
+        "imageURL" : null",
+        "course" : "Sides",
+        "id" : 23,
+        "price" : 4.31
+    },
+    {
+        "title": "Chips and Guacamole",
+        "description" : null,
+        "imageURL" : null",
+        "course" : "Sides",
+        "id" : 24,
+        "price" : 4.31
+    },
+    {
+        "title": "Crema",
+        "description" : null,
+        "imageURL" : null",
+        "course" : "Sides",
+        "id" : 25,
+        "price" : 0.71
+    },
+    {
+        "title": "Guacamole",
+        "description" : null,
+        "imageURL" : null",
+        "course" : "Sides",
+        "id" : 25,
+        "price" : 0.71
+    },
+    {
+        "title": "Queso",
+        "description" : null,
+        "imageURL" : null",
+        "course" : "Sides",
+        "id" : 25,
+        "price" : 3.71
+    },
+    {
+        "title": "Nopales Asados",
+        "description" : null,
+        "imageURL" : null",
+        "course" : "Sides",
+        "id" : 25,
+        "price" : 0.89
+    },
+    {
+        "title": "Cebollitas Asadas",
+        "description" : null,
+        "imageURL" : null",
+        "course" : "Sides",
+        "id" : 25,
+        "price" : 0.71
+    },
+    {
+        "title": "Ranch",
+        "description" : null,
+        "imageURL" : null",
+        "course" : "Sides",
+        "id" : 25,
+        "price" : 0.71
+    },
+    {
+        "title": "French Fries",
+        "description" : null,
+        "imageURL" : null",
+        "course" : "Sides",
+        "id" : 25,
+        "price" : 2.39
+    },
+    {
+        "title": "Carne Asada Fries",
+        "description" : null,
+        "imageURL" : null",
+        "course" : "Sides",
+        "id" : 25,
+        "price" : 9.59
+    },
+    {
+        "title": "Chuletas de Puerco",
+        "description" : null,
+        "imageURL" : null",
+        "course" : "Main Dishes",
+        "id" : 25,
+        "price" : 13.19
+    },
+    {
+        "title": "Plato de Cesina",
+        "description" : null,
+        "imageURL" : null",
+        "course" : "Main Dishes",
+        "id" : 25,
+        "price" : 13.19
+    },
+    {
+        "title": "Fajitas de Res",
+        "description" : null,
+        "imageURL" : null",
+        "course" : "Main Dishes",
+        "id" : 25,
+        "price" : 14.39
+    },
+    {
+        "title": "Taco Plate",
+        "description" : null,
+        "imageURL" : null",
+        "course" : "Main Dishes",
+        "id" : 25,
+        "price" : 7.91
+    },
+    {
+        "title": "Choripollo",
+        "description" : null,
+        "imageURL" : null",
+        "course" : "Main Dishes",
+        "id" : 25,
+        "price" : 13.91
+    },
+    {
+        "title": "Coctel de Camaron Chico",
+        "description" : null,
+        "imageURL" : null",
+        "course" : "Main Dishes",
+        "id" : 25,
+        "price" : 15.59
+    },
+    {
+        "title": "Coctel de Camaron Grande",
+        "description" : null,
+        "imageURL" : null",
+        "course" : "Main Dishes",
+        "id" : 25,
+        "price" : 17.99
+    },
+    {
+        "title": "Alambre",
+        "description" : null,
+        "imageURL" : null",
+        "course" : "Main Dishes",
+        "id" : 25,
+        "price" : 8.59
+    },
+    {
+        "title": "Puerco en Salsa Verde",
+        "description" : null,
+        "imageURL" : null",
+        "course" : "Main Dishes",
+        "id" : 25,
+        "price" : 13.19
+    },
+    {
+        "title": "Camarones ala Diabla",
+        "description" : null,
+        "imageURL" : null",
+        "course" : "Main Dishes",
+        "id" : 25,
+        "price" : 15.59
+    },
+    {
+        "title": "Fajitas de Pollo",
+        "description" : null,
+        "imageURL" : null",
+        "course" : "Main Dishes",
+        "id" : 25,
+        "price" : 13.19
+    },
+    {
+        "title": "Los Compadres",
+        "description" : null,
+        "imageURL" : null",
+        "course" : "Main Dishes",
+        "id" : 25,
+        "price" : 15.59
+    },
+    {
+        "title": "Costillas de Res Asadas",
+        "description" : null,
+        "imageURL" : null",
+        "course" : "Main Dishes",
+        "id" : 25,
+        "price" : 15.59
+    },
+    {
+        "title": "Filete ala Plancha",
+        "description" : null,
+        "imageURL" : null",
+        "course" : "Main Dishes",
+        "id" : 25,
+        "price" : 14.39
+    },
+    {
+        "title": "Carne Asada",
+        "description" : null,
+        "imageURL" : null",
+        "course" : "Main Dishes",
+        "id" : 25,
+        "price" : 13.19
+    },
+    {
+        "title": "Filete Empanizado",
+        "description" : null,
+        "imageURL" : null",
+        "course" : "Main Dishes",
+        "id" : 25,
+        "price" : 14.39
+    },
+    {
+        "title": "Caldo de Res",
+        "description" : null,
+        "imageURL" : null",
+        "course" : "Main Dishes",
+        "id" : 25,
+        "price" : 13.19
+    },
+    {
+        "title": "Mojarra",
+        "description" : null,
+        "imageURL" : null",
+        "course" : "Main Dishes",
+        "id" : 25,
+        "price" : 14.39
+    },
+    {
+        "title": "Fajitas Tejanas",
+        "description" : null,
+        "imageURL" : null",
+        "course" : "Main Dishes",
+        "id" : 25,
+        "price" : 14.39
+    },
+    {
+        "title": "Heart Plate",
+        "description" : null,
+        "imageURL" : null",
+        "course" : "Specials",
+        "id" : 25,
+        "price" : 14.39
+    },
+    {
+        "title": "Taco",
+        "description" : null,
+        "imageURL" : null",
+        "course" : "Main Dishes",
+        "id" : 25,
+        "price" : 1.99
+    },
+    {
+        "title": "Kids Quesadilla",
+        "description" : null,
+        "imageURL" : null",
+        "course" : "Kids",
+        "id" : 25,
+        "price" : 5.51
+    },
+    {
+        "title": "Kids Plate",
+        "description" : null,
+        "imageURL" : null",
+        "course" : "Kids",
+        "id" : 25,
+        "price" : 5.51
+    },
+    {
+        "title": "Jarritos",
+        "description" : null,
+        "imageURL" : null",
+        "course" : "Drinks",
+        "id" : 25,
+        "price" : 2.15
+    },
+    {
+        "title": "Jamaica",
+        "description" : null,
+        "imageURL" : null",
+        "course" : "Drinks",
+        "id" : 25,
+        "price" : 2.39
+    },
+    {
+        "title": "Horchata",
+        "description" : null,
+        "imageURL" : null",
+        "course" : "Drinks",
+        "id" : 25,
+        "price" : 2.39
+    },
+]
